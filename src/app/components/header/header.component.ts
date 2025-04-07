428ee1511db484977e6d4afe075664dc4b356261
Damian Boquete <damian.boquete@hotmail.com>
2025-04-07 16:23:38 +0200

    [MOD] added card effects

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -12,5 +12,29 @@ import { CommonModule } from '@angular/common';
 
 
 export class HeaderComponent {
+  // https://codepen.io/msriki12/pen/yyBNMEr
   @Input() owner!: Owner;
+
+  tilt(element: HTMLDivElement, event: MouseEvent) {
+    const force: number = 16;
+
+    const x: number = event.offsetX;
+    const y: number = event.offsetY;
+    const tiltX: number = (x - element.offsetWidth / 2) / force;
+    const tiltY: number = -(y - element.offsetHeight / 2) / force;
+
+    element.style.transform = `
+    rotateX(${tiltY}deg)
+    rotateY(${tiltX}deg)
+    `;
+
+    // element.style.setProperty("--gradient-opacity", pro);
+  }
+
+  reset(element: HTMLDivElement) {
+    element.style.transform = `
+    rotateX(0deg)
+    rotateY(0deg)
+    `;
+  }
 }
