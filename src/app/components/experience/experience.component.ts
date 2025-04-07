991fac954794c591c9575fd3925ada70a3dcbfd3
Damian Boquete <damian.boquete@hotmail.com>
2025-04-07 09:04:52 +0200

    [RM] remnoved map component as it became useless

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Experience } from '../../interfaces/experience';
 import { CommonModule } from '@angular/common';
-import { MapComponent } from "../map/map.component";
 
 
 @Component({
