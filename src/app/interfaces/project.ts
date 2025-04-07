2120a0eca9473d87de99b066ed9be5ca506248d0
Damian Boquete <damian.boquete@hotmail.com>
2025-04-07 16:25:03 +0200

    [RM] removed useless property

--- src/app/interfaces/project.ts
+++ src/app/interfaces/project.ts
@@ -1,6 +1,10 @@
+import { Technology } from "./technology";
+
 export interface Project {
     title: string;
     creationDate: string;
     description: string;
+    url: string;
+    techStack: Technology[];
     imageUrl: string;
 }
