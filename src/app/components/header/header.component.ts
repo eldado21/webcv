a076d92945a231525b70bc2898cddc0897595320
Damian Boquete <damian.boquete@hotmail.com>
2025-04-09 16:14:35 +0200

    [MOD] increased card tilt zone size

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -27,8 +27,6 @@ export class HeaderComponent {
     rotateX(${tiltY}deg)
     rotateY(${tiltX}deg)
     `;
-
-    // element.style.setProperty("--gradient-opacity", pro);
   }
 
   reset(element: HTMLDivElement) {
