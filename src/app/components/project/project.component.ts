e0500ba4d9f647006bf3829ae0fab7a44a25907d
ElSuperDado <damian.boquete@hotmail.com>
2025-11-18 15:56:21 +0100

    WIP on project card details

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -1,12 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
+import { NgClass } from '@angular/common';
 import { Project } from '../../interfaces/project';
 
 @Component({
   selector: 'app-project',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './project.component.html',
   styleUrl: './project.component.css'
 })
+
 export class ProjectComponent {
   @Input() project!: Project;
+  maximized: boolean = false;
+  growthIcon = signal("fa-solid fa-maximize");
+
+  toggleGrowth = (): void => {
+    this.growthIcon.set(this.maximized ? "fa-solid fa-maximize" : "fa-solid fa-minimize");
+    this.maximized = !this.maximized;
+    console.log(this.growthIcon());
+  }
 }
